7df7135864a80d84480aae89cb5dc6a7d232643c
Lukas Krecan <lukas.krecan@adleritech.net>
2017-11-08 15:33:40 +0100

    Extending InternalDocument

--- flexibee-core/src/main/java/com/adleritech/flexibee/core/api/domain/InternalDocument.java
+++ flexibee-core/src/main/java/com/adleritech/flexibee/core/api/domain/InternalDocument.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -28,6 +29,36 @@ public class InternalDocument {
     @Element(name = "datVyst", required = false)
     private LocalDate issued;
 
+    @Element(name = "duzpPuv", required = false)
+    private LocalDate timeOfSupply;
+
     @Element(name="varSym", required = false)
     private String variableSymbol;
+
+    @Element(name="cisDosle", required = false)
+    private String incomingNumber;
+
+    @Element(name="popis", required = false)
+    private String description;
+
+    @Element(name="sumOsv", required = false)
+    private BigDecimal vatFreeSum;
+
+    @Element(name="sumCelkem", required = false)
+    private BigDecimal totalSum;
+
+    @Element(name="mena", required = false)
+    private String currency;
+
+    @Element(name="primUcet", required = false)
+    private String primaryAccount;
+
+    @Element(name="protiUcet", required = false)
+    private String contraAccount;
+
+    @Element(name="zakazka", required = false)
+    private String order;
+
+    @Element(name="clenDph", required = false)
+    private String vatRow;
 }
