6e25738aa74d5efd2c521cd63bcce1a3ade34b4a
tbml <tibor.mlynarik@adleritech.com>
2018-06-19 12:18:02 +0200

    obligation items

--- flexibee-core/src/main/java/com/adleritech/flexibee/core/api/domain/Obligation.java
+++ flexibee-core/src/main/java/com/adleritech/flexibee/core/api/domain/Obligation.java
@@ -97,4 +97,8 @@ public class Obligation {
 
     @Element(name="stredisko", required = false)
     private String department;
+
+    @ElementList(name = "polozkyFaktury", required = false)
+    private ObligationItems items;
+
 }
